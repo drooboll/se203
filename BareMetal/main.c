28f9ca26aa299f7dc73ed3d01ed1ac336f5b4235
Valerii Drobotun <valerii-drobtun@telecom-paris.fr>
2021-10-02 01:35:16 +0700

    Fibonacci added

--- BareMetal/main.c
+++ BareMetal/main.c
@@ -1,15 +1,27 @@
 #include <stdint.h>
 
 uint8_t i = 0;
+
+uint_fast32_t fibo(uint_fast32_t n)
+{
+    if (!n)
+    {
+        return 0;
+    }
+    
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return fibo(n - 1) + fibo(n - 2);
+}
+
 int main()
 {
-    while (1)
-    {
-        if (i < 100)
-            i++;
-        else
-            i = 0;
-    }
+    uint_fast32_t test = fib(8);
+
+    while(1){}
 
     return 0;
 }
